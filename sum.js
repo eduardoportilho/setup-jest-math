df19e6c4bb3f295c242f36a0cd83ace703eeae9d
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-08-07 16:57:35 +0200

    Init

---
+++ sum.js
@@ -0,0 +1,8 @@
+module.exports = sum
+
+function sum(a, b) {
+  if (a == 7) {
+    return a
+  }
+  return a + b
+}
---
+++ test/test.js
@@ -0,0 +1,6 @@
+const sum = require('../sum')
+
+test('', () => {
+  const result = sum(1, 2)
+  expect(result).toBe(3)
+})
